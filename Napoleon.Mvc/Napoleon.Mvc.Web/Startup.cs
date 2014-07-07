fecaee5daad591cd6893a908183ec14d0cf9bc20
俞立钢 <dotnet100.lovems@gmail.com>
2014-07-07 22:05:06 +0800

    创建框架

---
+++ Napoleon.Mvc/Napoleon.Mvc.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Napoleon.Mvc.Web.Startup))]
+namespace Napoleon.Mvc.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
