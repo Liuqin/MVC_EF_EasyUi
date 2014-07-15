2d54bd6a92635bc2ee16d28c8607ee59f44d832f
俞立钢 <dotnet100.lovems@gmail.com>
2014-07-15 14:15:00 +0800

    二级菜单完成

---
+++ Napoleon.Mvc/Napoleon.Mvc.Web/Content/JS/Index.js
@@ -0,0 +1,22 @@
+﻿define(function (require, exports, module) {
+    //引入必须的css和js文件
+    require("jquery");
+
+    //#region 设置左边菜单的动画
+    /*设置活动样式*/
+    exports.SetSubmenu = function () {
+        $(".submenu li.active").removeClass("active");
+        $(this).addClass("active");
+    };
+    /*设置动画*/
+    exports.SlideMenu = function () {
+        var $ul = $(this).next('ul');
+        $('dd').find('ul').slideUp();
+        if ($ul.is(':visible')) {
+            $(this).next('ul').slideUp();
+        } else {
+            $(this).next('ul').slideDown();
+        }
+    };
+    //#endregion
+});
--- Napoleon.Mvc/Napoleon.Mvc.Web/Content/JS/PublicJs.js
+++ Napoleon.Mvc/Napoleon.Mvc.Web/Content/JS/PublicJs.js
@@ -1,9 +1,8 @@
-﻿
-
-define(function (require, exports, module) {
+﻿define(function (require, exports, module) {
     //引入必须的css和js文件
     require("jquery");
 
+    //#region 自定义的弹出框
     var confirmDiv, alertDiv;//遮罩层DIV
     confirmDiv = '<div id="confirm" style="width:100%;height:100%;z-index:99999; position:absolute;display:none; top:0px;background-color:rgba(0,0,0,0.5)!important; background:#000; filter:alpha(opacity=50); *background:#000; *filter:alpha(opacity=50);">' +
                     '<div class="tip">' +
@@ -39,15 +38,15 @@ define(function (require, exports, module) {
     /*使用示例：
      $.NpMessage({type:'alert',title:'123',msg:'123',onconfirm:function(){}}).show();
      */
-    exports.Message = function(options) {
+    exports.Message = function (options) {
         var option = $.extend({
             type: '', //输出框类型（confirm/alert）
             height: '100%',
             title: '', //标题
             msg: '', //显示的信息
-            onconfirm: function() {},
-            oncancel: function() {},
-            onclose: function() {}
+            onconfirm: function () { },
+            oncancel: function () { },
+            onclose: function () { }
         }, options);
 
         var that, body;
@@ -69,18 +68,18 @@ define(function (require, exports, module) {
         $(that).height(option.height);
         //初始化并赋值
         //右上角的关闭按钮，class=close
-        $(".close", that).click(function() {
+        $(".close", that).click(function () {
             option.onclose();
             this.hide(); //触发事件后隐藏掉
         }.bind(that));
         //确定按钮，input的class=confirm
-        $(".confirm", that).click(function() {
+        $(".confirm", that).click(function () {
             option.onconfirm();
             this.hide();
         }.bind(that));
         //取消按钮(询问框才会有)，input的class=cancel
         if (option.type === "confirm") {
-            $(".cancel", that).click(function() {
+            $(".cancel", that).click(function () {
                 option.oncancel();
                 this.hide();
             }.bind(that));
@@ -92,11 +91,24 @@ define(function (require, exports, module) {
         //最终显示图层
         return {
             box: that,
-            show: function() {
+            show: function () {
                 $(this.box).show();
                 return this;
             }
         };
     };
+    //#endregion
+
+    //#region 设置页面自动伸缩
+    exports.SetBody = function () {
+        var bodyWidth;
+        bodyWidth = document.documentElement.clientWidth;
+        if (bodyWidth < 950) {
+            $('body').css("width", "950px");
+        } else {
+            $('body').css("width", bodyWidth + "px");
+        }
+    };
+    //#endregion
 
 });
